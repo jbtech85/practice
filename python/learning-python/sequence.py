209abcef02e5b736ff139c64ec85055c4ed802b6
Josh Brooker <jbtech85@gmail.com>
2025-12-22 20:41:51 -0800

    uncomment

--- python/learning-python/sequence.py
+++ python/learning-python/sequence.py
@@ -1,21 +1,21 @@
 
 chaosList = [False, True, "Tree", 4.0, "Five", 6]
-# print(chaosList)
-# print(len(chaosList))
+print(chaosList)
+print(len(chaosList))
 
-# print(chaosList[1])
-# print(chaosList[-2])
+print(chaosList[1])
+print(chaosList[-2])
 
-# chaosList[0] = 0
+chaosList[0] = 0
 
-# print(chaosList)
+print(chaosList)
 
 
-# anotherList = [2,4,9]
-# chaosList += anotherList
-# print(chaosList)
+anotherList = [2,4,9]
+chaosList += anotherList
+print(chaosList)
 
-# chaosString = "Once upon a time"
-# print(chaosString[2])
+chaosString = "Once upon a time"
+print(chaosString[2])
 
 print(chaosList[1::2])
